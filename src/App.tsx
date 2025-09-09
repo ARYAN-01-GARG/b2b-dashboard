a9bc9f67f973e9ec7c8af6ce3bab1c25963a74fc
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-09-09 23:42:14 +0530

    Leads Management screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomeLayout from "@/components/home/HomeLayout"
 import Dashboard from "@/domain/b2b/dashboard/Dashboard"
 import LeadManagement from "./domain/b2b/leads-management/LeadManagement"
 import AddLead from "./domain/b2b/leads-management/AddLead"
+import MemberAndRevenue from "./domain/b2b/member-and-revenue/MemberAndRevenue"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route index element={<Dashboard />} />
         <Route path="gym-leads" element={<LeadManagement />} />
         <Route path="gym-leads/add" element={<AddLead />} />
+        <Route path="gym-members" element={<MemberAndRevenue />} />
         <Route path="*" element={<div className="flex items-center justify-center min-h-screen text-4xl">404 Not Found</div>} />
       </Route>
     </Routes>
